89cd4fd1acfdda231227771ce18757add476c183
Sergey Didanov <svdidanov@icloud.com>
2017-01-17 22:00:52 +0200

    Complete ServerInfoTableViewController

---
+++ NCloud.notes/HTTPClient.swift
@@ -0,0 +1,39 @@
+//
+//  HTTPClient.swift
+//  NCloud.notes
+//
+//  Created by Sergey Didanov on 01.01.17.
+//  Copyright © 2017 Sergey Didanov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+
+class HTTPClient {
+    let noteApiBaseURL = "/index.php/apps/notes/api/v0.2/notes"
+    
+    // Check connection to remote server using credentials as arguments
+    func checkServerConnUsing(server: String, username: String, password: String, checkConnectionHandler:@escaping (Bool) -> Void) {
+        var headers: HTTPHeaders = [:]
+        if let authorizationHeader = Request.authorizationHeader(user: username, password: password) {
+            headers[authorizationHeader.key] = authorizationHeader.value }
+        let requestURL = "https://" + server + noteApiBaseURL
+        print(requestURL)
+        Alamofire.request(requestURL, headers: headers).validate().responseJSON { response in
+            switch response.result {
+            case .success:
+                checkConnectionHandler(true)
+            case .failure:
+                checkConnectionHandler(false)
+            }
+        }
+    }
+    
+    func makeHttpRequest(url: String) {
+        
+    }
+}
+
+
--- NCloud.notes/ServerInfoTableViewController.swift
+++ NCloud.notes/ServerInfoTableViewController.swift
@@ -7,112 +7,134 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
 
 class ServerInfoTableViewController: UITableViewController {
-
     @IBOutlet weak var connectionActivityIndecator: UIActivityIndicatorView!
+    @IBOutlet weak var serverNameTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var connectionStatusButton: UILabel!
+    @IBOutlet weak var allowSelfSignCertSwitch: UISwitch!
+    let connectionStatus = UserDefaults.standard
+    var httpClient: HTTPClient!
+    var isLoggedIn = false {
+        didSet {
+            if isLoggedIn {
+                connectionStatusButton.text = "Disconnect"
+                if let serverName = KeychainWrapper.standard.string(forKey: "server"), let userName = KeychainWrapper.standard.string(forKey: "username"), let password = KeychainWrapper.standard.string(forKey: "password") {
+                    serverNameTextField.text = serverName
+                    userNameTextField.text = userName
+                    passwordTextField.text = password
+                }
+            } else {
+                connectionStatusButton.text = "Connect"
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.title = "Server information"
         connectionActivityIndecator.isHidden = true
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        // Check if app is logged in
+        if UserDefaults.standard.object(forKey: "loggedIn") as? Bool != nil {
+            isLoggedIn = true
+        }
+        // Detect if user want to use Self Signed Certificate
+        allowSelfSignCertSwitch.addTarget(self, action: #selector(ServerInfoTableViewController.switchChanged(_:)), for: UIControlEvents.valueChanged)
     }
-
+    
+    // If user want to use Self Signed Certificate, show alert and disable switch
+    func switchChanged(_ mySwitch: UISwitch) {
+        if mySwitch.isOn {
+            showAlert(withMessage: "Self Signed Certificates disabled in iOS from January 2017.")
+            allowSelfSignCertSwitch.setOn(false, animated: true)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         switch section {
-        case 0:
-            return 4
-        case 1:
-            return 1
-        default:
-            return 0
+        case 0: return 4
+        case 1: return 1
+        default: return 0
         }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        // Click "Connect" button
+        // Click "Connect/Disconnect" button
         if indexPath.section == 1 {
-            connectionActivityIndecator.isHidden = false
-            connectionActivityIndecator.startAnimating()
-            print("Start connection")
+            if !isLoggedIn {
+                // Click "Connect" button
+                if (serverNameTextField.text?.characters.count)! > 0 && (userNameTextField.text?.characters.count)! > 0 && (passwordTextField.text?.characters.count)! > 0 {
+                    // Start and show connectionActivityIndecator
+                    self.connectionActivityIndecator.startAnimating(); connectionActivityIndecator.isHidden = false; connectionActivityIndecator.startAnimating()
+                    httpClient = HTTPClient()
+                    httpClient.checkServerConnUsing(server: serverNameTextField.text!, username: userNameTextField.text!, password: passwordTextField.text!) { connectionStatus in
+                        switch connectionStatus {
+                        case true:
+                            self.showAlert(withMessage: "Connection successfull")
+                            self.saveServerCredentials()
+                        case false:
+                            self.showAlert(withMessage: "Server information is incorrect!")
+                        }
+                    }
+                } else {
+                    // One of the fields is empty
+                    showAlert(withMessage: "Server information is incorrect!")
+                }
+            } else {
+                // Click "Disconnect" button
+                removeServerCredentials()
+            }
+            // Stop and hide connectionActivityIndecator
+            self.connectionActivityIndecator.stopAnimating(); connectionActivityIndecator.isHidden = true; connectionActivityIndecator.stopAnimating()
         }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    // Show alert using message as argument
+    func showAlert(withMessage message: String) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // Click "Connect" button with successfull connection
+    func saveServerCredentials() {
+        KeychainWrapper.standard.set(serverNameTextField.text!, forKey: "server")
+        KeychainWrapper.standard.set(userNameTextField.text!, forKey: "username")
+        KeychainWrapper.standard.set(passwordTextField.text!, forKey: "password")
+        connectionStatus.set(true, forKey: "loggedIn")
+        isLoggedIn = true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    // Click "Disconnect" button
+    func removeServerCredentials() {
+        connectionStatus.removeObject(forKey: "loggedIn")
+        KeychainWrapper.standard.removeObject(forKey: "server")
+        KeychainWrapper.standard.removeObject(forKey: "username")
+        KeychainWrapper.standard.removeObject(forKey: "password")
+        serverNameTextField.text = ""
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+        isLoggedIn = false
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    deinit {
+        print("ServerInfoTableViewController deinited")
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
